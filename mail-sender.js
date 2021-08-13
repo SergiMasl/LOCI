c54641df48700453b36300d3c83950aed59fc66b
Maxim Kunaev <maksim.kunaev@westarp.io>
2021-08-14 01:41:11 +0300

    disabled server

--- mail-sender.js
+++ mail-sender.js
@@ -1,56 +1,56 @@
-const BOSS = process.env.BOSS;
-const PASS = process.env.PASS;
-const RESIVER = process.env.RESIVER;
+// const BOSS = process.env.BOSS;
+// const PASS = process.env.PASS;
+// const RESIVER = process.env.RESIVER;
 
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-let transporter = null;
+// let transporter = null;
 
-async function main() {
-  transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: BOSS,
-      pass: PASS,
-    },
-  });
-}
-console.log(BOSS, PASS, RESIVER);
-main().catch(console.error);
+// async function main() {
+//   transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: BOSS,
+//       pass: PASS,
+//     },
+//   });
+// }
+// console.log(BOSS, PASS, RESIVER);
+// main().catch(console.error);
 
-async function sendEmail(data) {
-  return 'ok'
-  // console.log(data)
-  // console.log(BOSS)
-  // console.log(PASS)
-  // console.log(RESIVER)
-  // console.log('\n')
+// async function sendEmail(data) {
+//   return 'ok'
+//   // console.log(data)
+//   // console.log(BOSS)
+//   // console.log(PASS)
+//   // console.log(RESIVER)
+//   // console.log('\n')
 
-  // const options = {
-  //   from: `"Manager" <${BOSS}>`,
-  //   to: RESIVER,
-  //   subject: "Client message from website",
-  //   text: data.text,
-  //   html: `<p>
-  //     <b>First Name:</b> ${data.firstName}.<br>
-  //     <b>Last Name:</b> ${data.fastName}.<br>
-  //     <b>Phone:</b> ${data.mobile}.<br>
-  //     <b>Email:</b> ${data.email}.<br>
-  //     <b>Text:</b>  ${data.inputText}.</br>
-  //   </p>`,
-  // };
+//   // const options = {
+//   //   from: `"Manager" <${BOSS}>`,
+//   //   to: RESIVER,
+//   //   subject: "Client message from website",
+//   //   text: data.text,
+//   //   html: `<p>
+//   //     <b>First Name:</b> ${data.firstName}.<br>
+//   //     <b>Last Name:</b> ${data.fastName}.<br>
+//   //     <b>Phone:</b> ${data.mobile}.<br>
+//   //     <b>Email:</b> ${data.email}.<br>
+//   //     <b>Text:</b>  ${data.inputText}.</br>
+//   //   </p>`,
+//   // };
 
-  // try {
-  //   let info = await transporter.sendMail(options);
-  //   console.log("Message sent: %s", info.messageId);
-  //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // try {
+//   //   let info = await transporter.sendMail(options);
+//   //   console.log("Message sent: %s", info.messageId);
+//   //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-  //   return info.messageId;
-  // } catch (error) {
-  //   throw new Error(error);
-  // }
-}
+//   //   return info.messageId;
+//   // } catch (error) {
+//   //   throw new Error(error);
+//   // }
+// }
 
-module.exports = {
-  sendEmail,
-};
+// module.exports = {
+//   sendEmail,
+// };
--- server.js
+++ server.js
@@ -1,62 +1,62 @@
-require("dotenv").config({ path: __dirname + "/.env" });
-const cors = require("cors");
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const path = require("path");
-const http = require("http");
-const app = express();
-const { sendEmail } = require("./mail-sender");
-const jsonParser = bodyParser.json();
-const allowedOrigins = ["http://localhost:8080", "https://localhost:8080"];
+// require("dotenv").config({ path: __dirname + "/.env" });
+// const cors = require("cors");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const fs = require("fs");
+// const path = require("path");
+// const http = require("http");
+// const app = express();
+// const { sendEmail } = require("./mail-sender");
+// const jsonParser = bodyParser.json();
+// const allowedOrigins = ["http://localhost:8080", "https://localhost:8080"];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin." +
-        origin;
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       const msg =
+//         "The CORS policy for this site does not " +
+//         "allow access from the specified Origin." +
+//         origin;
 
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-    exposedHeaders: [
-      "Content-Length",
-      "Accept",
-      "Accept-Encoding",
-      "Accept-Language",
-      "Access-Control-Request-Headers",
-      "Access-Control-Request-Method",
-      "Cache-Control",
-      "Connection",
-      "Host",
-      "Origin",
-      "Pragma",
-      "Referer",
-      "Sec-Fetch-Mode",
-      "Sec-Fetch-Site",
-      "User-Agent",
-    ],
-  })
-);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//     exposedHeaders: [
+//       "Content-Length",
+//       "Accept",
+//       "Accept-Encoding",
+//       "Accept-Language",
+//       "Access-Control-Request-Headers",
+//       "Access-Control-Request-Method",
+//       "Cache-Control",
+//       "Connection",
+//       "Host",
+//       "Origin",
+//       "Pragma",
+//       "Referer",
+//       "Sec-Fetch-Mode",
+//       "Sec-Fetch-Site",
+//       "User-Agent",
+//     ],
+//   })
+// );
 
-app.post("/api/send-mail", jsonParser, async (req, response) => {
-  try {
-    const data = await sendEmail(req.body);
-    return response.status(200).json({ message: "success", data });
-  } catch (error) {
-    console.log(error);
-    return response.status(400).json({ message: error.message });
-  }
+// app.post("/api/send-mail", jsonParser, async (req, response) => {
+//   try {
+//     const data = await sendEmail(req.body);
+//     return response.status(200).json({ message: "success", data });
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(400).json({ message: error.message });
+//   }
 
-  return response.status(400).json({ message: "error" });
-});
+//   return response.status(400).json({ message: "error" });
+// });
 
-const httpServer = http.createServer(app);
-httpServer.listen(3000, () => console.log("HTTP started on port 3000"));
+// const httpServer = http.createServer(app);
+// httpServer.listen(3000, () => console.log("HTTP started on port 3000"));
 
 
